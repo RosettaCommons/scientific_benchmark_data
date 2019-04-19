12679264b045ee9191fab176ff5d4d122a969fa2
Jeliazko Jeliazkov <jeliazkov@jhu.edu>
2019-04-19 16:45:31 -0400

    remove some auxillary files

--- antibody_grafting/command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#antibody.macosclangrelease -fasta $1.truncated.fasta -antibody:grafting_database ~/Rosetta/tools/antibody-update -no_relax -antibody:exclude_pdb $1 -antibody:n_multi_templates 1
-antibody.macosclangrelease -fasta $1.truncated.fasta -no_relax -antibody:exclude_pdb $1 -antibody:n_multi_templates 1
